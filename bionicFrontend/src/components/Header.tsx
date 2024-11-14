9d68bd50b82314eaf2f67e6f1516881e2bb08801
Alistair McCann <allyjmccann@gmail.com>
2024-11-14 21:08:56 +0100

    added pictures and trying to sort layout

--- bionicFrontend/src/components/Header.tsx
+++ bionicFrontend/src/components/Header.tsx
@@ -3,9 +3,9 @@ import '../components/styles/header.css'
 function Header() {
     return (
         <div className='header -container'>
-            <img className='header-logo' src="./bionicFrontend/src/assets/logo.svg" alt="company logo" />
+            <img className='header-logo' src="./src/assets/logo.svg" alt="company logo" />
             <h1 className="header-h1">TakewAway</h1>
-            <img src="./bionicFrontend/src/asets/kundkorg.svg" alt="basket symbol" className="kundkorg" />
+            <img src="./src/assets/kundkorg.svg" alt="basket symbol" className="kundkorg" />
             <button className='login-btn'>login btn</button>
 
         </div>
