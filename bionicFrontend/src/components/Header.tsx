9b3b1086e3418f185f0a5e6db95c6fda0289749a
Alistair McCann <allyjmccann@gmail.com>
2024-11-14 20:06:26 +0100

    test

--- bionicFrontend/src/components/Header.tsx
+++ bionicFrontend/src/components/Header.tsx
@@ -4,11 +4,9 @@ function Header() {
     return (
         <div className='header -container'>
             <img className='header-logo' src="./bionicFrontend/src/assets/logo.svg" alt="company logo" />
-            <img className='header-image' src="./bionicFrontend/src/assets/headerImg.png" alt="header image" />
             <h1 className="header-h1">TakewAway</h1>
             <img src="./bionicFrontend/src/asets/kundkorg.svg" alt="basket symbol" className="kundkorg" />
-
-            <button>login</button>
+            <button className='login-btn'>login btn</button>
 
         </div>
     )
