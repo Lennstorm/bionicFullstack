ad1af1978deab9d3779c538cf1cc5081132c6483
Alistair McCann <allyjmccann@gmail.com>
2024-11-14 22:05:26 +0100

    some final adjustments but still some minor fixes needed

--- bionicFrontend/src/components/Header.tsx
+++ bionicFrontend/src/components/Header.tsx
@@ -1,12 +1,22 @@
-import '../components/styles/header.css'
+import '../components/styles/header.css';
+import headerImg from '../assets/headerImg.png';
 
 function Header() {
     return (
-        <div className='header -container'>
+        <div style={{
+            background: `linear-gradient(to right, white 20%, rgba(255, 255, 255, 0) 70%), url(${headerImg})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }} className='header-container'>
+
             <img className='header-logo' src="./src/assets/logo.svg" alt="company logo" />
-            <h1 className="header-h1">TakewAway</h1>
+
+            <h1 className="header-h1">TakeAway</h1>
+
             <img src="./src/assets/kundkorg.svg" alt="basket symbol" className="kundkorg" />
             <button className='login-btn'>login btn</button>
+
+
 
         </div>
     )
