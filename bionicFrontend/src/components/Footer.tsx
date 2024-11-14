7e87fdaada4bf3cdfdd04691d2384ae5cfd78edd
Alistair McCann <allyjmccann@gmail.com>
2024-11-14 12:09:46 +0100

    added more code but have to wait for routing in order to test everything

--- bionicFrontend/src/components/Footer.tsx
+++ bionicFrontend/src/components/Footer.tsx
@@ -2,7 +2,23 @@ import '../components/styles/footer.css'
 
 function Footer() {
     return (
-        <div>
+        <div className='footer-container'>
+            <section>
+                <h3 className="footer-text">
+                    Södra Torggatan 16
+                </h3>
+            </section>
+            <section>
+                <img className='footer-social' src="" alt="" />
+                <img className='footer-social' src="" alt="" />
+                <img className='footer-social' src="" alt="" />
+            </section>
+            <section>
+                <img className='footer-telephone' src="" alt="" />
+                <h3 className="footer-text">
+                    010-4983020
+                </h3>
+            </section>
 
         </div>
     )
