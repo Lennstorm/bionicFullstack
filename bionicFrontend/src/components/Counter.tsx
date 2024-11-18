55964d8da343b886777ea7c2b372f96de469edaf
Alistair McCann <allyjmccann@gmail.com>
2024-11-18 13:05:39 +0100

    small adjustments to css and ts code

--- bionicFrontend/src/components/Counter.tsx
+++ bionicFrontend/src/components/Counter.tsx
@@ -5,31 +5,28 @@ const Counter = () => {
     const [count, setCount] = useState<number>(0);
 
     const increment = () => setCount(count + 1);
-    
+
     const decrement = () => {
         if (count > 0) {
             setCount(count - 1);
         }
-
     }
 
 
     return (
 
         <div className='counter-container'>
-            <section >
-                <button className='counter-button' onClick={decrement}>
-                    -
-                </button>
-            </section>
-            <section className='count'>
+            <button className='counter-button' onClick={decrement}>
+                -
+            </button>
+
+            <section className='count' id='count'>
                 {count}
             </section>
-            <section >
-                <button className='counter-button' onClick={increment}>
-                    +
-                </button>
-            </section>
+
+            <button className='counter-button' onClick={increment}>
+                +
+            </button>
         </div>
     )
 }
