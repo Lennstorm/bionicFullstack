bb3f8b8455f2b23c4a402333437863a088fbbc29
Lennstorm <4824136@gmail.com>
2024-11-26 22:44:35 +0100

    validateAddUser

---
+++ bionicBackend/src/middleware/validateAddUser.js
@@ -0,0 +1,12 @@
+const validateUser = (schema) => {
+    return {
+        before: (handler) => {
+            const {error } = schema.validate(handler.event.body);
+            if (error) {
+                throw new Error(error.details[0].message);                
+            }            
+        },
+    };
+};
+
+module.exports = { validateUser };
