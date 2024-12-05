157824b01299d19928b9552a24fd30b7253d2529
Peter Andersson <karlstad88@hotmail.com>
2024-12-05 20:44:20 +0100

    waiterpage mitt i

--- bionicFrontend/src/interfaces.ts
+++ bionicFrontend/src/interfaces.ts
@@ -15,5 +15,20 @@ export interface MenuItems {
    popularIndex: number,
    image: string
 
-
 }
+
+export interface OrderContent {
+   count: number;
+   specialRequest: string;
+   menuItemID: string;
+ }
+ 
+ export interface OrderItem {
+   orderItemID: string;
+   editedAt: string; // Om du använder Date, kan detta justeras till Date
+   orderLocked: boolean;
+   orderStatus: string;
+   userID: string;
+   createdAt: string; // Om du använder Date, kan detta justeras till Date
+   orderContent: OrderContent[];
+ }
